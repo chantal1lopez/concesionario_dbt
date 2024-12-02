246e12f9d60cf9cea250f677813b5d93f510a997
Chantal Lopez <chantylopez1@gmail.com>
2024-12-02 17:03:26 +0000

    new things in different areas

---
+++ models/marts/finance/branch_finance.sql
@@ -0,0 +1,21 @@
+{{ config(
+    materialized='table',
+    schema='concesionario',
+    database='ALUMNO24_DEV_GOLD_DB'
+) }}
+
+WITH branch_finance AS (
+    SELECT
+        b.branch_id,
+        b.branch_name,
+        DATE_TRUNC('month', s.sale_date) AS period,
+        SUM(s.final_price_usd) AS total_revenue,
+        SUM(s.base_price_usd) AS total_cost,
+        SUM(s.final_price_usd - s.base_price_usd) AS total_margin
+    FROM {{ ref('fact_sales') }} s
+    LEFT JOIN {{ ref('dim_branch') }} b
+        ON s.branch_id = b.branch_id
+    WHERE s.is_active = TRUE
+    GROUP BY b.branch_id, b.branch_name, DATE_TRUNC('month', s.sale_date)
+)
+SELECT * FROM branch_finance
---
+++ models/marts/operations/mechanic_performance.sql
@@ -0,0 +1,15 @@
+WITH mechanic_performance AS (
+    SELECT
+        mec.mechanic_id,
+        mec.mechanic_name,
+        COUNT(srv.service_id) AS total_services,
+        SUM(srv.service_cost_usd) AS total_service_cost,
+        AVG(srv.service_cost_usd) AS avg_service_cost
+    FROM {{ ref('fact_services') }} srv
+    LEFT JOIN {{ ref('dim_mechanics') }} mec
+        ON srv.mechanic_id = mec.mechanic_id
+    WHERE srv.is_active = TRUE
+    GROUP BY
+        mec.mechanic_id, mec.mechanic_name
+)
+SELECT * FROM mechanic_performance
